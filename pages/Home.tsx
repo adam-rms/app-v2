692068809f19eb1c6e2838ca1f0b4cbdfbcae3ef
John Cherry <github@johncherry.me>
2023-05-28 11:34:07 +0100

    Fix infinite re-renders

--- pages/Home.tsx
+++ pages/Home.tsx
@@ -11,6 +11,13 @@ const Home = ({
 }: DrawerScreenProps<RMSDrawerParamList, "Home">) => {
   // Close the drawer - it sometimes gets stuck open when navigating back to this page
   navigation.closeDrawer();
+  // Reset navigation - some routes get a bit confused and this is a fallback assertion.
+  if (navigation.canGoBack()) {
+    navigation.reset({
+      index: 0,
+      routes: [{ name: "Home" }],
+    });
+  }
   return (
     <Container maxW="full" alignItems="center" m="2">
       <Heading>Welcome to AdamRMS</Heading>
